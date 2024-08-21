b15544d485da20665470844d125101551ed3d20b
tomlrd <thomas.laroudie@gmail.com>
2024-08-21 17:04:39 +0200

    1.0.1

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Heart } from "lucide-react";
+import { useTranslation } from "react-i18next";
 
 export default function Footer() {
+  const { t } = useTranslation();
+
   return (
     <footer className="w-full bg-orange-500 text-white text-center py-1">
       <div className="flex items-baseline justify-center ">
         <p className="flex items-center">
-          © 2024 made by myself with{" "}
-          <Heart color="red" size={15} className="mx-1 fill-red-600 " /> All
-          rights reserved.
+          {t("footer1")}
+          <Heart color="red" size={15} className="mx-1 fill-red-600 " />
+          {t("footer2")}
         </p>
       </div>
     </footer>
