8f401aa811da64f0331cc44ef525ae9420b86016
tomlrd <thomas.laroudie@gmail.com>
2024-08-21 22:44:11 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
+import "./output.css";
 import Header from "./components/Header";
 import Main from "./pages/Main";
 import Profile from "./pages/Profile";
