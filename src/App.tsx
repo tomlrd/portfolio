6356e224158e792ad8ecf5fdf17796d15f9fa591
tomlrd <thomas.laroudie@gmail.com>
2024-08-28 20:42:05 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,11 @@ function App() {
         <Header />
         <div className="flex-grow flex justify-center md:px-16 dark:bg-[--background-gray]">
           <Routes>
-            <Route path="/portfolio" element={<Main />} />
-            <Route path="/portfolio/projects" element={<Projects />} />
-            <Route path="/portfolio/profile" element={<Profile />} />
-            <Route path="/portfolio/electron" element={<Electron />} />
-            <Route path="/portfolio/contact" element={<Contact />} />
+            <Route path="/" element={<Main />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/electron" element={<Electron />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
         <Footer />
